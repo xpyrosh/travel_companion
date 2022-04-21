7e7bd4e3626ab90dceda54fcaaa78ce00044b702
Shiva Ramsamoojm <shiva.ramsamooj@hotmail.com>
2022-04-21 15:35:40 -0400

    added google map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import GoogleMapReact from 'google-map-react'
 import {Paper, Typography, useMediaQuery} from '@material-ui/core'
-// import LocationOutlinedIcon from "@material-ui/icons/LocationOutlined"
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined"
 import Rating from '@material-ui/lab'
+import "./map.css"
 
 const Map = () => {
+  const coordinates = {lat: 29.293127, lng: -94.878584}
   
-
   return (
-    <h1>Map</h1>
+
+    // https://stackoverflow.com/questions/46566830/how-to-use-create-react-app-with-an-older-react-version
+    // Needed older React for GoogleMapReact
+    <div className="mapContainer">
+      <GoogleMapReact
+        bootstrapURLKeys={{key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY}}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50,50,50,50]}
+        >
+
+      </GoogleMapReact>
+    </div>
   )
 }
 
